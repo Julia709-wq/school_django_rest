3ef4dd437db11513c9c7e5f68ea7ab5a70f61ed5
Julia <yulia.2018.ivanova@gmail.com>
2025-11-13 01:40:03 +0300

    users' rights determined

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls', namespace='school')),
-    path('', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.permissions import AllowAny
 
-from users.serializers import PaymentSerializer
-from users.models import Payment
+from users.serializers import PaymentSerializer, UserSerializer
+from users.models import Payment, User
 
 
 class PaymentListAPIView(ListAPIView):
@@ -15,3 +16,12 @@ class PaymentListAPIView(ListAPIView):
     search_fields = ['payment_method']
     ordering_fields = ('date',)
 
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
